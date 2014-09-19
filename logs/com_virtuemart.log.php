603e96370cfbbea0b07161a181751167f08c2631
lenard112 <lenard.latte@khk.ee>
2014-09-20 02:00:15 +0300

    Basic configuration

---
+++ logs/com_virtuemart.log.php
@@ -0,0 +1,7 @@
+#
+#<?php die("Forbidden."); ?>
+
+%2014-%09-%19 %21:%Sep:%th ERROR vmError: Was not able to save the VirtueMart user data
+%2014-%09-%19 %21:%Sep:%th ERROR vmError: Was not able to save the VirtueMart user data
+%2014-%09-%19 %21:%Sep:%th ERROR vmError: Was not able to save the VirtueMart user data
+%2014-%09-%19 %21:%Sep:%th ERROR vmError: Was not able to save the VirtueMart user data
